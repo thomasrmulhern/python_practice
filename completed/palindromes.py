860bfdbcdc9216a3cf4ae03e65547794e04eb632
Thomas Mulhern <mulhern.thomasr@gmail.com>
2018-09-28 11:23:02 -0700

    functions return True instead of just printing the result

--- completed/palindromes.py
+++ completed/palindromes.py
@@ -12,8 +12,10 @@ def is_pal1(word):
     s_word = re.sub(r'[^\w\s]','',s_word)
     if s_word == s_word[::-1]:
         print(word, 'is a palindrome')
+        return True
     else:
         print(word, 'is not a palindrome')
+        return False
 
 
 def is_pal2(word):
@@ -31,10 +33,14 @@ def is_pal2(word):
     if len(s_word) % 2 == 0:
         if s_word[:(len(s_word)//2)] == s_word[(len(s_word)//2):][::-1]:
             print(word,  'is a palindrome')
+            return True
         else:
             print(word,  'is not a palindrome')
+            return False
     else:
         if s_word[:((len(s_word)-1)//2)] == s_word[((len(s_word)-1)//2)+1:][::-1]:
             print(word, 'is a palindrome')
+            return True
         else:
             print (word, 'is not a palindrome')
+            return False
