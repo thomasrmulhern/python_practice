2fa517437819a2c1d77ae04dcd4aa06c4bf59738
Thomas Mulhern <mulhern.thomasr@gmail.com>
2018-09-28 12:09:37 -0700

    added a function that check if a string is a palindrome by checking opposit characters for half the length of the string; currently tring to refactor it

--- completed/palindromes.py
+++ completed/palindromes.py
@@ -11,12 +11,11 @@ def is_pal1(word):
     s_word = word.strip(' ').lower().replace(' ', '')
     s_word = re.sub(r'[^\w\s]','',s_word)
     if s_word == s_word[::-1]:
-        print(word, 'is a palindrome')
+        print(word, 'IS a palindrome')
         return True
     else:
-        print(word, 'is not a palindrome')
+        print(word, 'is NOT a palindrome')
         return False
-
 
 def is_pal2(word):
     '''
@@ -32,15 +31,44 @@ def is_pal2(word):
     s_word = re.sub(r'[^\w\s]','',s_word)
     if len(s_word) % 2 == 0:
         if s_word[:(len(s_word)//2)] == s_word[(len(s_word)//2):][::-1]:
-            print(word,  'is a palindrome')
+            print(word,  'IS a palindrome')
             return True
         else:
-            print(word,  'is not a palindrome')
+            print(word,  'is NOT a palindrome')
             return False
     else:
         if s_word[:((len(s_word)-1)//2)] == s_word[((len(s_word)-1)//2)+1:][::-1]:
-            print(word, 'is a palindrome')
+            print(word, 'IS a palindrome')
             return True
         else:
-            print (word, 'is not a palindrome')
+            print (word, 'is NOT a palindrome')
             return False
+
+def is_pal3(word):
+    '''
+    check if string is palindrome checking opposite character pairs for half the length of the string
+    Examples:
+    tacocat == True
+    taco cat == True
+    A Toyota! Race fast, safe car! A Toyota! == True
+    soccer == False
+    '''
+    import re
+    s_word = word.strip(' ').lower().replace(' ', '')
+    s_word = re.sub(r'[^\w\s]','',s_word)
+    if len(s_word) % 2 == 0:
+        for _ in range(len(s_word)//2):
+            for x,y in zip(s_word, s_word[::-1]):
+                if x != y:
+                    print(word, 'is NOT a palindrome')
+                    return False
+        print(word, 'IS a palindrome')
+        return True
+    else:
+        for _ in range((len(s_word)-1)//2):
+            for x,y in zip(s_word, s_word[::-1]):
+                if x != y:
+                    print(word, 'is NOT a palindrome')
+                    return False
+        print(word, 'IS a palindrome')
+        return True
