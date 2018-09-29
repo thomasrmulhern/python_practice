385d70fb7bc2cc9a055f922fd1e32762a9a83f13
Thomas Mulhern <mulhern.thomasr@gmail.com>
2018-09-29 15:24:37 -0700

    started restaurant recommender problem

---
+++ in_progress/restaurant_recc.py
@@ -0,0 +1,49 @@
+# Georgie Porgie
+# 87%
+# $$$
+# Canadian, Pub Food
+#
+# Queen St. Cafe
+# 82%
+# $
+# Malaysian, Thai
+#
+# Dumplings R Us
+# 71%
+# $
+# Chinese
+#
+# Mexican Grill
+# 85%
+# $$
+# Mexican
+#
+# Deep Fried Everything
+# 52%
+# $
+# Pub Food
+
+names_to_rating = {
+'Georgie Porgie': 87,
+'Queen St. Cafe': 82,
+'Dumplings R Us': 71,
+'Mexican Grill': 85,
+'Deep Fried Everything': 52
+}
+
+
+price_to_name = {
+'$': ['Queen St. Cafe, Dumplings R Us, Deep Fried Everything'],
+'$$': ['Mexican Grill'],
+'$$$': ['Georgie Porgie'],
+'$$$$': []
+}
+
+ cuisine_to_names = {
+'Canadian': ['Georgie Porgie'],
+'Pub Food': ['Georgie Porgie', 'Deep Fried Everything'],
+'Malaysian': ['Queen St. Cafe'],
+'Thai': ['Queen St. Cafe'],
+'Chinese': ['Dumplings R Us'],
+'Mexican': ['Mexican Grill']
+}
